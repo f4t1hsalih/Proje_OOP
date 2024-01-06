aee704725d5f0418aca49ce07fcca84714a60e38
f4t1hsalih <f4t1hsalih@gmail.com>
2024-01-06 23:41:24 +0300

    Ders 8 - Metot Örnekleri

---
+++ Proje_OOP/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proje_OOP.Examples;
+
+namespace Proje_OOP.Controllers
+{
+    public class DefaultController : Controller
+    {
+        void Topla()
+        {
+            int sayi1;
+            int sayi2;
+            sayi1 = 3;
+            sayi2 = 4;
+            int toplam = sayi1 + sayi2;
+        }
+        void Islemler()
+        {
+            Class1 c = new Class1();
+            c.Topla();
+        }
+    }
+}
--- Proje_OOP/Examples/Class1.cs
+++ Proje_OOP/Examples/Class1.cs
@@ -2,11 +2,30 @@
 
 namespace Proje_OOP.Examples
 {
-    //Sınıfların içindeki öğelere erişim sağlamak için
-    //sınıftan bir nesne türetilir
-    //SınıfAdı nesneadı = new SınıfAdı();
     public class Class1
     {
-        Product product = new Product();
+        void metot()
+        {
+            //Sınıfların içindeki öğelere erişim sağlamak için
+            //sınıftan bir nesne türetilir
+            //SınıfAdı nesneadı = new SınıfAdı();
+            //Çağrılan sınıfın içindeki değerlere ulaşabilmek için sınıfı metot içinde çağırmak gerekmektedir
+            Product p = new Product();
+            p.Name = "Test";
+        }
+
+        public void Topla()
+        {
+            int sayi1 = 10;
+            int sayi2 = 20;
+            int sonuc = sayi1 + sayi2;
+
+        }
+        void Carp()
+        {
+            int s1 = 10;
+            int s2 = 20;
+            int sonuc = s1 * s2;
+        }
     }
 }
