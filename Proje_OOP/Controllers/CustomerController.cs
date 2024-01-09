f83e5c9e1e4c348bd1bed3552a44e8d55a703501
f4t1hsalih <f4t1hsalih@gmail.com>
2024-01-10 00:37:47 +0300

    Ders 24 - Müşterilere Ait Contreller Metodlarının Yazılması

---
+++ Proje_OOP/Controllers/CustomerController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proje_OOP.Entity;
+using Proje_OOP.ProjeContext;
+
+namespace Proje_OOP.Controllers
+{
+    public class CustomerController : Controller
+    {
+        Context context = new Context();
+
+        //Listeleme İşlemi
+        public IActionResult Index()
+        {
+            var values = context.Customers.ToList();
+            return View(values);
+        }
+
+        //Kaydetme İşlemi
+        //Get işlemi gerçekleşince yapılacaklar
+        [HttpGet]
+        public IActionResult AddCustomer()
+        {
+            return View();
+        }
+        //Post işlemi gerçekleşince yapılacaklar
+        [HttpPost]
+        public IActionResult AddCustomer(Customer c)
+        {
+            //c den gelen değerleri ekler
+            context.Add(c);
+            //Değişiklikleri kaydeder
+            context.SaveChanges();
+            //Index Action'una (Index Sayfası) yönlendirir
+            return RedirectToAction("Index");
+        }
+
+        //Silme İşlemi
+        public IActionResult DeleteCustomer(int id)
+        {
+            //Silinecek değeri alıp value isimli değişkene atar
+            var value = context.Customers.Where(x => x.CustomerID == id).FirstOrDefault();
+            //Değeri siler
+            context.Remove(value);
+            //Değişiklikleri kaydeder
+            context.SaveChanges();
+            //Index sayfasına yönlendirir
+            return RedirectToAction("Index");
+        }
+
+        //Güncelleme İşlemi
+        //Get işlemi gerçekleşince yapılacaklar
+        [HttpGet]
+        public IActionResult UpdateCustomer(int id)
+        {
+            var value = context.Customers.Where(x => x.CustomerID == id).FirstOrDefault();
+            return View(value);
+        }
+        //Post işlemi gerçekleşince yapılacaklar
+        [HttpPost]
+        public IActionResult UpdateCustomer(Customer c)
+        {
+            //Üzerinde değişiklik yapılacak veririyi alır
+            var value = context.Customers.Where(x => x.CustomerID == c.CustomerID).FirstOrDefault();
+            //Verinin CustomerCity Prop'una Sınıftan gelen(Güncelleme sayfasından gelen) değeri yazar
+            value.CustomerCity = c.CustomerCity;
+            //Verinin CustomerName Prop'una Sınıftan gelen(Güncelleme sayfasından gelen) değeri yazar
+            value.CustomerName = c.CustomerName;
+            //Değişiklikleri kaydeder
+            context.SaveChanges();
+            //Index Sayfasına yönlendirir
+            return RedirectToAction("Index");
+        }
+    }
+}
