18a41a18d6d47cf568882d18bf3458d4bf818aa6
f4t1hsalih <f4t1hsalih@gmail.com>
2024-01-09 16:04:10 +0300

    Ders 19 - Products List

---
+++ Proje_OOP/Controllers/ProductContreller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proje_OOP.ProjeContext;
+
+namespace Proje_OOP.Controllers
+{
+    public class ProductContreller : Controller
+    {
+        Context context= new Context();
+
+        public IActionResult Index()
+        {
+            var values =context.Products.ToList();
+            return View(values);
+        }
+    }
+}
